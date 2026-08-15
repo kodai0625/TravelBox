5c3951ac948d335c7c447de77337fd76b98c6419
kodai0625 <koudai4869conan@gmail.com>
2026-08-16 03:43:17 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * 中身が変わる＝端末が「新しい世話係だ」と気づく合図なので、
  * ここを消したり固定したりしないでください。
  */
-const VERSION = '20260815-2231-11287c88';
+const VERSION = '20260816-0313-5d92bb64';
 const CACHE = 'travel-box-' + VERSION;
 
 self.addEventListener('install', (e) => {
