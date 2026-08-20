0e93138d0bcefb477fb467707638a80c35ef06ea
kodai0625 <306216695+kodai0625@users.noreply.github.com>
2026-08-21 02:07:55 +0900

    更新 2026-08-21 02:07

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * 中身が変わる＝端末が「新しい世話係だ」と気づく合図なので、
  * ここを消したり固定したりしないでください。
  */
-const VERSION = 'dcdb75fb';
+const VERSION = 'b756fe3d';
 const CACHE = 'travel-box-' + VERSION;
 
 self.addEventListener('install', (e) => {
