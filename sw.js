8781a2f555c8c1953cd70c644daa3c8c86f6911b
kodai0625 <306216695+kodai0625@users.noreply.github.com>
2026-08-20 16:50:13 +0900

    玄関の説明文と手順書を直した

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * 中身が変わる＝端末が「新しい世話係だ」と気づく合図なので、
  * ここを消したり固定したりしないでください。
  */
-const VERSION = '5352f803';
+const VERSION = 'dcdb75fb';
 const CACHE = 'travel-box-' + VERSION;
 
 self.addEventListener('install', (e) => {
